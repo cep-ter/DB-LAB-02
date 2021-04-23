e1083665eadf078dc4d90f14e3f0702d059714f5
Sepehr Rafiee <sepehr.rafiee@snapp.cab>
2021-04-23 12:16:44 +0430

    fix(src): fix app.controller login

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiResponse } from '@nestjs/swagger';
 import { AuthService } from './auth/auth.service';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { Public } from './public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private authService: AuthService) {}
 
+  @ApiResponse({ status: 200, description: 'Login user' })
+  @Header('Content-Type', 'application/json')
+  @Public()
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
