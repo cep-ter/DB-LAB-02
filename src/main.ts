556dea79c6eb14ada0cbaaf267c1f07af4ecd5b5
Sepehr Rafiee <sepehr.rafiee@snapp.cab>
2021-04-23 11:02:44 +0430

    fix(main.ts): fix swagger title and descriptions

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const options = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('DB LAB')
+    .setDescription('The LAB 02 API description')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('db')
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
